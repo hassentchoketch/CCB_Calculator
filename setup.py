b8b6a40099385b083a08cdde6a55ec6534f86caa
Hacene Tchoketch-kebir <89703595+hassentchoketch@users.noreply.github.com>
2023-07-12 17:11:28 +0100

    update Files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='ccb_calculator',
+    version='1.0',
+    author='Hacene Tchoketch-Kebir',
+    # packages=['ccb_calculator'],
+    packages=['CCB_Calculator'],
+    install_requires=[
+        'numpy',
+        'statsmodels'
+    ],
+)
